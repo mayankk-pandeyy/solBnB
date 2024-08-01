9ce7482fc0439c8b4c1c017bb89882c51cbdc792
Mayank Pandey <immayank7781@gmail.com>
2024-08-01 23:57:25 +0530

    updated code

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,9 +1,7 @@
 import React from 'react'
-import Slider from '@mui/material/Slider';
 
 
 const Slider = () => {
-
   return (
     <div className='w-[100%]'>
         {/* Top */}
@@ -12,21 +10,10 @@ const Slider = () => {
         </div>
 
         {/* Bottom */}
-        <div className='w-full h-[10px] bg-[#FD395C] rounded-2xl'>
-          <Slider
-            aria-label="Small steps"
-            defaultValue={0.00000005}
-            getAriaValueText={"6,743,190"}
-            step={0.00000001}
-            marks
-            min={-0.00000005}
-            max={0.0000001}
-            valueLabelDisplay="auto"
-          />
-        </div>
+        <div className='w-full h-[10px] bg-[#FD395C] rounded-2xl'></div>
 
     </div>
   )
 }
 
-export default Sliderr
+export default Slider
