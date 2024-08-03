11255945b3f58b81982a5ba471271dc9905e5bc1
Mayank Pandey <immayank7781@gmail.com>
2024-08-04 04:31:00 +0530

    Update HeroMobile.js

--- src/components/HeroMobile.js
+++ src/components/HeroMobile.js
@@ -39,7 +39,7 @@ const img2y = {
 
 const img3x = {
     visible : {opacity : 1, x: 0},
-    hidden : {opacity : 0, x : -400}
+    hidden : {opacity : 0, x : -800}
 }
 
 const img3y = {
@@ -52,119 +52,174 @@ const rightBox = {
     hidden: { opacity: 0, y: 500 },
 }
 
+
+const data = {
+    ETH: {
+      ETH: 0.3,
+      USDT: 0.1,
+      USDC: 0.09
+    },
+    BSC: {
+      ETH: 0.3,
+      USDT: 0.1,
+      USDC: 0.09
+    },
+    SOL: {
+      ETH: 0.3,
+      USDT: 0.1,
+      USDC: 0.09
+    },
+  };
+  
+
 const HeroMobile = () => {
+
+
+    const [pay, setPay] = useState("ETH");
+    const [recieve, setRecieve] = useState("ETH");
+    const [payIcon, setPayIcon] = useState(eth);
+    const [recieveIcon, setRecieveIcon] = useState(eth);
+    const [value, setValue] = useState(0);
+    const [alert, setAlert] = useState(false);
+    const [payValue, setPayValue] = useState("");
+    let [exchanged, setExchanged] = useState("");
+
+    function payHandler(event) {
+        const payment = event.target.value;
+        setPayValue(event.target.value);
+        setValue(payment);
+    
+      }
+      
+      useEffect(()=> {
+        let ans = (data[pay][recieve] * value);
+        setExchanged(ans);
+      }, [payValue])
+
+
+    function submitHandler(){
+        setAlert(true);
+    }
+
+    function closeAlertHandler(){
+        setAlert(false);
+    }
+
+
+
   return (
-    <div className='w-full'>
-        {/* Top */}
-        <div className='w-full relative flex justify-end'>
-            <motion.div className=''
-                initial="hidden"
-                animate="visible"
-                variants={solbnb}
-                transition={{
-                    duration: 0.5,
-                    delay : 1
-                }}
-            >
-                <img src={logo1} width={300}/>
-            </motion.div>
-
-            {/* img1 */}
-            <motion.div 
+    <div className='w-[full]'>
+        <div className="bg-gradient-to-r from-[#F9F9F9] mx-2 rounded-2xl">
+            {/* Top */}
+            <div className='w-full relative flex justify-end pr-4 '>
+                <motion.div className=''
                     initial="hidden"
                     animate="visible"
-                    variants={img1x}
-                    transition={{
-                        duration: 1,
-                        delay : 1
-                    }}
-                    className='absolute left-16 bottom-4'
-                >
-                <motion.img src={image1} width={120}
-                    initial="hidden"
-                    animate="visible"
-                    variants={img1y}
-                    transition={{
-                    duration: 0.5,
-                    delay : 1
-                    }}
-                />
-            </motion.div>
-
-            {/* Img 2 */}
-            <motion.div 
-                    initial="hidden"
-                    animate="visible"
-                    variants={img2x}
+                    variants={solbnb}
                     transition={{
                         duration: 0.5,
                         delay : 1
                     }}
-                    className='absolute left-40 bottom-32'
                 >
-                <motion.img src={image2} width={80}
+                    <img src={logo1} width={200}/>
+                </motion.div>
+
+                {/* img1 */}
+                <motion.div 
+                        initial="hidden"
+                        animate="visible"
+                        variants={img1x}
+                        transition={{
+                            duration: 1,
+                            delay : 1
+                        }}
+                        className='absolute left-8 bottom-0'
+                    >
+                    <motion.img src={image1} width={120}
+                        initial="hidden"
+                        animate="visible"
+                        variants={img1y}
+                        transition={{
+                        duration: 0.5,
+                        delay : 1
+                        }}
+                    />
+                </motion.div>
+
+                {/* Img 2 */}
+                <motion.div 
+                        initial="hidden"
+                        animate="visible"
+                        variants={img2x}
+                        transition={{
+                            duration: 0.5,
+                            delay : 1
+                        }}
+                        className='absolute left-24 bottom-20'
+                    >
+                    <motion.img src={image2} width={100}
+                        initial="hidden"
+                        animate="visible"
+                        variants={img2y}
+                        transition={{
+                            duration: 1,
+                            delay : 1
+                        }}
+                    />
+                </motion.div>
+
+                {/* Img 3 */}
+
+                <motion.div 
+                        initial="hidden"
+                        animate="visible"
+                        variants={img3x}
+                        transition={{
+                            duration: 0.4,
+                            delay : 1
+                        }}
+                        className='absolute left-32 -top-10'
+                    >
+                    <motion.img src={image3} width={80}
+                        initial="hidden"
+                        animate="visible"
+                        variants={img3y}
+                        transition={{
+                            duration: 0.8,
+                            delay : 1
+                        }}
+                    />
+                </motion.div>
+            </div>
+
+
+            {/* Middle Section */}
+            <div>
+                <motion.div className='text-5xl text-[#FF385C] mt-4 font-bold ml-10'
                     initial="hidden"
                     animate="visible"
-                    variants={img2y}
+                    variants={solbnb}
                     transition={{
-                        duration: 1,
-                        delay : 1
+                            duration: 1,
                     }}
-                />
-            </motion.div>
-
-            {/* Img 3 */}
-
-            <motion.div 
-                    initial="hidden"
-                    animate="visible"
-                    variants={img3x}
-                    transition={{
-                        duration: 0.4,
-                        delay : 1
-                    }}
-                    className='absolute left-44 top-2'
                 >
-                <motion.img src={image3} width={80}
+                    SolBnB.
+                </motion.div>
+                <motion.div className='text-4xl font-bold ml-10'
                     initial="hidden"
                     animate="visible"
-                    variants={img3y}
+                    variants={solbnb}
                     transition={{
-                        duration: 0.8,
-                        delay : 1
+                        duration: 1.2,
                     }}
-                />
-            </motion.div>
+                >
+                    Tokenized Airbnb <br/> Partnerships on <br/> Solana
+                </motion.div>
+            </div>
         </div>
-
-
-        {/* Middle Section */}
-        <div>
-            <motion.div className='text-5xl text-[#FF385C] mt-4 font-bold ml-10'
-                initial="hidden"
-                animate="visible"
-                variants={solbnb}
-                transition={{
-                        duration: 1,
-                }}
-            >
-                SolBnB.
-            </motion.div>
-            <motion.div className='text-4xl font-bold ml-10'
-                initial="hidden"
-                animate="visible"
-                variants={solbnb}
-                transition={{
-                    duration: 1.2,
-                }}
-            >
-                Tokenized Airbnb <br/> Partnerships on <br/> Solana
-            </motion.div>
-        </div>
-
         {/* Bottom Section */}
-        <div className='w-[80%] mx-auto h-full overflow-hidden mt-5'>
-            <motion.div className='w-full border-[2px] h-full px-10 py-4 rounded-2xl flex flex-col items-center gap-3'
+        <div className='w-[95%] mx-auto h-full overflow-hidden mt-5'>
+            <motion.div className='w-full border-[2px] h-full px-8 py-4 rounded-2xl flex flex-col items-center gap-3'
                 initial="hidden"
                 animate="visible"
                 variants={rightBox}
@@ -209,38 +264,42 @@ const HeroMobile = () => {
                     1 $SOLBNB = $0.000722
                 </div>
 
-                <div className='w-full flex justify-between px-4'>
-                    <div className='flex gap-2 items-center'>
-                        <img src={eth} width={25}/>
-                        <span>ETH</span>
+                <div>
+
+                    <div className='w-full flex justify-between'>
+                        <div className='flex gap-2 items-center px-2 py-1 rounded-t-xl cursor-pointer' onClick={()=>{setPay("ETH"); setPayIcon(eth)}} style={{backgroundColor : `${pay}` === "ETH" ? ("#000000") : (""), color : `${pay}` === "ETH" ? ("#ffffff") : ("")}}>
+                            <img src={eth} width={25}/>
+                            <span>ETH</span>
+                        </div>
+
+                        <div className='flex gap-2 items-center px-2 py-1 rounded-t-xl cursor-pointer' onClick={()=>{setPay("BSC"); setPayIcon(bsc)}} style={{backgroundColor : `${pay}` === "BSC" ? ("#000000") : (""), color : `${pay}` === "BSC" ? ("#ffffff") : ("")}}>
+                            <img src={bsc} width={25} />
+                            <span>BSC</span>
+                        </div>
+
+                        <div className='flex gap-2 items-center px-2 py-1 rounded-t-xl cursor-pointer' onClick={()=>{setPay("SOL"); setPayIcon(sol)}} style={{backgroundColor : `${pay}` === "SOL" ? ("#000000") : (""), color : `${pay}` === "SOL" ? ("#ffffff") : ("")}}>
+                            <img src={sol} width={25}/>
+                            <span>SOL</span>
+                        </div>
                     </div>
-
-                    <div className='flex gap-2 items-center'>
-                        <img src={bsc} width={25} />
-                        <span>BSC</span>
-                    </div>
-
-                    <div className='flex gap-2 items-center'>
-                        <img src={sql} width={25}/>
-                        <span>SOL</span>
-                    </div>
-                </div>
+                    
 
 
-                <div className='w-full flex justify-between border-[1.5px] border-black py-2 px-4 rounded-full'>
-                    <div className='flex gap-2 items-center'>
-                        <img src={eth} width={25}/>
-                        <span>ETH</span>
-                    </div>
+                    <div className='w-full flex justify-between border-[1.5px] border-black py-2 px-4 rounded-3xl' style={{borderTopLeftRadius : pay === ("ETH") ? ('0px') : (''), borderTopRightRadius : pay === "SOL" ? ('0px') : (''), }}>
+                        <div className='flex gap-2 items-center px-2 py-1 rounded-full cursor-pointer' onClick={()=>{setRecieve("ETH"); setRecieveIcon(eth)}} style={{backgroundColor : `${recieve}` === "ETH" ? ("#FD395C") : ("")}}>
+                            <img src={eth} width={25}/>
+                            <span>ETH</span>
+                        </div>
 
-                    <div className='flex gap-2 items-center'>
-                        <img src={bsc} width={25} />
-                        <span>BSC</span>
-                    </div>
+                        <div className='flex gap-2 items-center px-2 py-1 rounded-full cursor-pointer' onClick={()=>{setRecieve("USDT"); setRecieveIcon(bsc)}} style={{backgroundColor : `${recieve}` === "USDT" ? ("#FD395C") : ('')}}>
+                            <img src={bsc} width={25} />
+                            <span>USDT</span>
+                        </div>
 
-                    <div className='flex gap-2 items-center'>
-                        <img src={sql} width={25}/>
-                        <span>SQL</span>
+                        <div className='flex gap-2 items-center px-2 py-1 rounded-full cursor-pointer' onClick={()=>{setRecieve("USDC"); setRecieveIcon(sol)}} style={{backgroundColor : `${recieve}` === "USDC" ? ("#FD395C") : ("")}}>
+                            <img src={sol} width={25}/>
+                            <span>USDC</span>
+                        </div>
                     </div>
                 </div>
 
@@ -253,7 +312,7 @@ const HeroMobile = () => {
                             <span className='text-xs font-bold text-[#FD395C]'>Max</span>
                         </div>
                         <div className='flex border-[1.5px] border-black rounded-full'>
-                            <input type='numbers' placeholder='0' className='w-[80%] bg-transparent pl-3 text-[#6A6A6A]'/>
+                            <input type='number' placeholder='0' className='w-[80%] bg-transparent pl-3 text-[#6A6A6A] outline-none' onChange={payHandler}/>
                             <img src={eth} width={25}/>
                         </div>
                     </div>
@@ -264,16 +323,32 @@ const HeroMobile = () => {
                             <span className='text-xs font-bold'>Receive $SOLBNB</span>
                         </div>
                         <div className='flex border-[1.5px] border-black rounded-full'>
-                            <input type='numbers' placeholder='0' className='w-[80%] bg-transparent pl-3 text-[#6A6A6A]'/>
-                            <img src={logo1} width={15}/>
+                            <div type='number' placeholder='0' className='w-[80%] bg-transparent pl-3 text-[#6A6A6A] outline-none overflow-hidden'>{exchanged}</div>
+                            <img src={recieveIcon} width={25}/>
                         </div>
                     </div>
                 </div>
 
                 <button className='w-full bg-[#FD395C] text-center rounded-full py-3 text-sm font-bold text-white'
+                    onClick={submitHandler}
                 >
                     Connect wallet
                 </button>
+
+                {/* Alert */}
+
+                {/* Alert Box */}
+                <div className='fixed top-0 left-9'>
+                    {
+                        alert ? (
+                            <div className='rounded-3xl w-[350px] h-[150px] bg-slate-900 py-4 px-3 text-xl font-bold text-white text-center'>
+                                <div>{`You have selected ${value} ${pay} which is equal to ${exchanged} ${recieve}`}</div>
+                                
+                                <button className='bg-red-500 cursor-pointer px-3 py-1 rounded-xl mt-4' onClick={closeAlertHandler}>CLOSE</button>
+                            </div>
+                        ) : ("")
+                    }
+                </div>
 
             </motion.div>
         </div>
